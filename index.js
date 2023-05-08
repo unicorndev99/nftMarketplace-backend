bc6f1c6c35f71156345c87d0b00673c42c13318b
unicorndev99 <aadumaharramli111@gmail.com>
2023-05-08 21:16:40 +0300

    change live port to 3001

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
 });
 app.use('/', require('./routers/getRoute'),);
 
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
     console.log(`Server is running at the port ${port}`);
